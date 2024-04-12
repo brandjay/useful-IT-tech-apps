3beef3f8a9ca2c6291ed34a92dcacf80b6115d38
Brandon <brandon101morgan@outlook.com>
2024-04-11 20:22:41 -0500

    QuikTek .01
    QuikTek is an app that controls the whole computer and more from one app. Used to speed up day to day task

---
+++ QuikTek/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace QuikTek
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            control = new Button();
+            restartbtn = new Button();
+            sysinfo = new Button();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // control
+            // 
+            control.Location = new Point(12, 12);
+            control.Name = "control";
+            control.Size = new Size(124, 29);
+            control.TabIndex = 0;
+            control.Text = "control panel";
+            control.UseVisualStyleBackColor = true;
+            control.Click += control_Click;
+            // 
+            // restartbtn
+            // 
+            restartbtn.Location = new Point(12, 381);
+            restartbtn.Name = "restartbtn";
+            restartbtn.Size = new Size(124, 29);
+            restartbtn.TabIndex = 1;
+            restartbtn.Text = "Restart PC";
+            restartbtn.UseVisualStyleBackColor = true;
+            restartbtn.Click += restartbtn_Click;
+            // 
+            // sysinfo
+            // 
+            sysinfo.Location = new Point(165, 12);
+            sysinfo.Name = "sysinfo";
+            sysinfo.Size = new Size(124, 29);
+            sysinfo.TabIndex = 2;
+            sysinfo.Text = "systeminfo";
+            sysinfo.UseVisualStyleBackColor = true;
+            sysinfo.Click += button1_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 73);
+            button1.Name = "button1";
+            button1.Size = new Size(115, 29);
+            button1.TabIndex = 3;
+            button1.Text = "ending script";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(324, 12);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 29);
+            button2.TabIndex = 4;
+            button2.Text = "whats my IP?";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(sysinfo);
+            Controls.Add(restartbtn);
+            Controls.Add(control);
+            Name = "Form1";
+            Text = "Quiktek";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button control;
+        private Button restartbtn;
+        private Button sysinfo;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ QuikTek/Form1.cs
@@ -0,0 +1,77 @@
+using System.Diagnostics;
+using System.Net;
+
+namespace QuikTek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Calls for the system info application
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("msinfo32.exe");
+        }
+        //Calls the control panel
+        private void control_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("control.exe");
+        }
+        //Restarts computer. To shutdown PC remove the /r(restart) and replace with /s for shutdown
+        private void restartbtn_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure you want to restart the computer?", "Confirmation", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                // the /r stands for restart and /s will shutdown 
+                var psi = new ProcessStartInfo("shutdown", "/r /t 0");
+                psi.CreateNoWindow = true;
+                psi.UseShellExecute = false;
+                Process.Start(psi);
+            }
+        }
+        // this uses the underlying method used in the ipconfig command. It finds its own IP that has been assigned via DHCP
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string textToCopy = "This ticket will remain open for 1 more day. If no reply this ticket will be considered reolved and closed";
+            Clipboard.SetText(textToCopy);
+            MessageBox.Show("Script copied");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string hostName = Dns.GetHostName();
+
+            // Get the IP address
+            IPAddress[] ipAddresses = Dns.GetHostAddresses(hostName);
+            //must be null to accept that not having an IP may be an option
+            IPAddress ipAddress = null;
+
+            // Find the first IPv4 address
+            foreach (IPAddress ip in ipAddresses)
+            {
+                if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    ipAddress = ip;
+                    break;
+                }
+            }
+
+            // Show the IP address in a message box
+            if (ipAddress != null)
+            {
+                MessageBox.Show("IP Address: " + ipAddress.ToString());
+            }
+            else
+            {
+                MessageBox.Show("No IPv4 address found. You may not be online. Please check your internet connection or VPN connection.");
+            }
+            //copy IP to clipboard and let user know they can know paste it as a response
+            string copymyIP = ipAddress.ToString();
+            Clipboard.SetText(copymyIP);
+            MessageBox.Show("Your IP is copied to clipboard.");
+        }
+    }
+}
