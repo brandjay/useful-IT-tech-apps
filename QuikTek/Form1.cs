eb72756f17c95743bcbc0e5abb706f3cc6606a93
Brandon <brandon101morgan@outlook.com>
2024-08-12 22:11:27 -0600

    Added a somewhat useful function

--- QuikTek/Form1.cs
+++ QuikTek/Form1.cs
@@ -1,5 +1,13 @@
+using Microsoft.VisualBasic.ApplicationServices;
 using System.Diagnostics;
 using System.Net;
+using System.IO;
+using System.IO.Compression;
+using System.Windows.Forms;
+using System;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.DirectoryServices;
 
 namespace QuikTek
 {
@@ -32,19 +40,13 @@ namespace QuikTek
                 Process.Start(psi);
             }
         }
-        // this uses the underlying method used in the ipconfig command. It finds its own IP that has been assigned via DHCP
-
-
         private void button2_Click(object sender, EventArgs e)
         {
             string hostName = Dns.GetHostName();
-
             // Get the IP address
             IPAddress[] ipAddresses = Dns.GetHostAddresses(hostName);
             //must be null to accept that not having an IP may be an option
             IPAddress ipAddress = null;
-
-            // Find the first IPv4 address
             foreach (IPAddress ip in ipAddresses)
             {
                 if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
@@ -53,7 +55,6 @@ namespace QuikTek
                     break;
                 }
             }
-
             // Show the IP address in a message box
             if (ipAddress != null)
             {
@@ -68,7 +69,6 @@ namespace QuikTek
             Clipboard.SetText(copymyIP);
             MessageBox.Show("Your IP is copied to clipboard.");
         }
-
         private void button3_Click(object sender, EventArgs e)
         {
             DialogResult result = MessageBox.Show("Are you sure you want to shutdown the computer?", "Confirmation", MessageBoxButtons.YesNo);
@@ -78,21 +78,91 @@ namespace QuikTek
                 var psi = Process.Start("shutdown", "/s /t 0");
             }
         }
-
         private void button4_Click(object sender, EventArgs e)
         {
             try
             {
                 string url = textBox1.Text;
-                //Uri uri = new Uri(url);
                 IPAddress[] addresses = Dns.GetHostAddresses(url);
 
                 linkLabel1.Text = "IP Address: " + addresses[0].ToString();
+
+                string ipAddress = addresses[0].ToString();
+                Clipboard.SetText(ipAddress);
+                MessageBox.Show(ipAddress + "\n IP is copied to clipboard ");
             }
             catch (Exception ex)
             {
                 MessageBox.Show("Error: " + ex.Message);
             }
         }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog ProfileSnatch = new FolderBrowserDialog())
+            {
+                // be sure to add the last "\" else it sleects the current logged in user for some stupid reason 
+                ProfileSnatch.SelectedPath = @"C:\Users\";
+                //Opens the file explorer 
+                DialogResult result = ProfileSnatch.ShowDialog();
+                //choose the profile to transfer 
+                string selectedDirectory = ProfileSnatch.SelectedPath;
+                DialogResult choice = MessageBox.Show("You selected: " + selectedDirectory, "Directory Selected", MessageBoxButtons.YesNoCancel);
+                if (result == DialogResult.OK && choice == DialogResult.Yes)
+                {
+                    string destinationFolder = @"C:\Atesting\"; // Change this to your desired destination folder
+                    // Change name of compressed folder to directory name.zip
+                    string zipFileName = Path.GetFileName(selectedDirectory) + ".zip";
+                    string zipFilePath = Path.Combine(destinationFolder, zipFileName);
+
+                    
+                    // Zip the selected directory
+                    try
+                    {
+                        ZipFile.CreateFromDirectory(selectedDirectory, zipFilePath);
+                    }
+                    catch (UnauthorizedAccessException ex)
+                    {
+                        MessageBox.Show("Access denied: " + ex.Message);
+                    }
+                    catch (IOException ex)
+                    {
+                        MessageBox.Show("I/O error: " + ex.Message);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("An error occurred: " + ex.Message);
+                    }
+                   // check for hidden folder and skip over them
+                   
+                    //set directory permissions 
+                    try
+                    {
+                        DirectoryInfo directory = new DirectoryInfo(selectedDirectory); 
+                        DirectorySecurity security = directory.GetAccessControl();
+                        security.AddAccessRule(new FileSystemAccessRule(@"yourdomainor-or-pcname if local\User", FileSystemRights.FullControl, AccessControlType.Allow));
+                        directory.SetAccessControl(security);
+                    }
+                    catch (UnauthorizedAccessException ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+
+                    // set path of zip folder to a string 
+                    string destinationPath = Path.Combine(destinationFolder, Path.GetFileName(zipFilePath));
+                    // Transfer the zipped file to the destination folder
+                    File.Move(zipFilePath, destinationPath);
+                    //
+                    MessageBox.Show("Directory zipped and transferred successfully!" + zipFilePath);
+                }
+                // if user decides they got the wrong folder then they can reset the dialog back to the profiles upon clicking no or cancel
+                else
+                {
+                    ProfileSnatch.ShowDialog(this);
+                }
+            }
+           
+        }
+
     }
+   
 }
